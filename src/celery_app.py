583e0e0c83362845b2a1de04b6ae3fb4e4ad9464
Disney <ivan36samsonov@gmail.com>
2025-07-07 22:10:14 +0300

    change broker url in celery_app.py file

--- src/celery_app.py
+++ src/celery_app.py
@@ -1,10 +1,12 @@
 # src/celery_app.py
 from celery import Celery
 from celery.schedules import crontab
+from src.config import settings
 
 celery_app = Celery(
     'lego_collection',
-    broker='pyamqp://guest@localhost//',  # Можно заменить на redis://... если используешь Redis
+    # Формируем URL брокера из настроек окружения
+    broker=f"pyamqp://{settings.RABBITMQ_USER}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}//",
     backend='rpc://',
     include=[
         'src.tournaments.tasks',
